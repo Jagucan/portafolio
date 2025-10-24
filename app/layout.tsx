b255c9be2bba0aee5384e4882352fb7e58524c21
Jagucan <jjgc.2499@gmail.com>
2025-10-24 13:52:41 -0500

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,29 +19,29 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
-	title: "Jacinto Gutiérrez | Desarrollador de Software",
-	title: "Portafolio de Jacinto Gutiérrez | Desarrollador FullStack & AR/VR",
-  	description:
-    	"Portafolio profesional de Jacinto Gutiérrez, desarrollador de software especializado en soluciones web, móviles y experiencias de realidad aumentada y virtual.",
-  	keywords: [
-    	"Jacinto Gutiérrez",
-    	"Jacinto José Gutiérrez Cantillo",
-    	"portafolio Jacinto Gutiérrez",
-    	"desarrollador fullstack Jacinto Gutiérrez",
-    	"desarrollador AR/VR Jacinto Gutiérrez",
-   		"desarrollador fullstack",
-   		"portafolio desarrollador",
-    	"desarrollo web",
-    	"desarrollo móvil",
-    	"realidad aumentada",
-    	"realidad virtual",
-    	"programador .NET",
-    	"Node.js",
-    	"React",
-    	"Angular",
-    	"AR/VR",
-    	"portafolio profesional"
-  	]};
+	title: "Jacinto Gutiérrez | Desarrollador FullStack & AR/VR",
+	description:
+		"Portafolio profesional de Jacinto Gutiérrez, desarrollador de software especializado en soluciones web, móviles y experiencias de realidad aumentada y virtual.",
+	keywords: [
+		"Jacinto Gutiérrez",
+		"Jacinto José Gutiérrez Cantillo",
+		"portafolio Jacinto Gutiérrez",
+		"desarrollador fullstack Jacinto Gutiérrez",
+		"desarrollador AR/VR Jacinto Gutiérrez",
+		"desarrollador fullstack",
+		"portafolio desarrollador",
+		"desarrollo web",
+		"desarrollo móvil",
+		"realidad aumentada",
+		"realidad virtual",
+		"programador .NET",
+		"Node.js",
+		"React",
+		"Angular",
+		"AR/VR",
+		"portafolio profesional",
+	],
+};
 
 export default function RootLayout({
 	children,
